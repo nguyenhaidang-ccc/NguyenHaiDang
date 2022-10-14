8a331e4f93aceb7f6556d24620e6898611be1d7d
admin <admin@EDWFTTGW>
2022-10-14 08:18:22 +0700

    Add project files.

---
+++ NguyenHaiDang/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using NguyenHaiDang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NguyenHaiDang.Models;
+
+namespace NguyenHaiDang.Controllers
+{
+    public class ProductController : Controller
+    {
+        WebBanVaLiEntities db = new WebBanVaLiEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult QuocGiaPartial()
+        {
+            List<tQuocGia> lstQG = db.tQuocGias.ToList();
+            return PartialView(lstQG);
+        }
+    }
+}
---
+++ NguyenHaiDang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenHaiDang.MvcApplication" Language="C#" %>
